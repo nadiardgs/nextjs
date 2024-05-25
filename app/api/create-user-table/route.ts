0a42592636ff32a240597b5e5f44fbae4852e3e4
Nadia Rodrigues <nadia.rodrigues93@gmail.com>
2024-05-26 00:42:39 +0100

    create user table

---
+++ app/api/create-user-table/route.ts
@@ -0,0 +1,12 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request: Request) {
+  try {
+    const result =
+      await sql`CREATE TABLE User (Id SERIAL PRIMARY KEY, Name VARCHAR(25) NOT NULL, Password VARCHAR(25) NOT NULL);`;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
